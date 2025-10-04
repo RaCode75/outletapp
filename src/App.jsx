5880866365477512c7bd3e0e3248b7feeb1cc301
RaCode75 <racode75@gmail.com>
2025-10-04 06:25:02 -0300

    Some styles aplied

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import Header from './components/Header'
+import Nav from './components/Nav'
+
 
 function App() {
 
 
   return (
-
+    <>
+    <Header />
+    <Nav />
+    </>
   )
 }
 
